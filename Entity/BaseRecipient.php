4fac65fe07d213015696b451ad125e9f4d701e69
Torge Rothe <tr@braune-digital.com>
2019-10-08 13:10:44 +0200

    Removed IdAwareTrait from BaseRecipient

--- Entity/BaseRecipient.php
+++ Entity/BaseRecipient.php
@@ -2,7 +2,6 @@
 
 namespace BrauneDigital\MessageBundle\Entity;
 
-use App\Model\IdAwareTrait;
 use Gedmo\Mapping\Annotation as Gedmo;
 use Gedmo\SoftDeleteable\Traits\SoftDeleteable;
 use Gedmo\Timestampable\Traits\TimestampableEntity;
@@ -11,7 +10,6 @@ use Gedmo\Timestampable\Traits\TimestampableEntity;
  * @Gedmo\SoftDeleteable()
  */
 abstract class BaseRecipient {
-    use IdAwareTrait;
     use SoftDeleteable;
     use TimestampableEntity;
 
